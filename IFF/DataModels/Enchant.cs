ff860c9865cc7be24160559f973515ad0632e3bc
Andreas N <git@pixelde.su>
2018-11-07 23:32:55 +0100

    IFF: Add parsing of Enchant files

---
+++ IFF/DataModels/Enchant.cs
@@ -0,0 +1,11 @@
+using System.Runtime.InteropServices;
+
+namespace IFF.DataModels {
+
+  [StructLayout (LayoutKind.Sequential, Pack = 4)]
+  public struct Enchant {
+    public uint Active;
+    public uint ID;
+    public uint Price;
+  }
+}
--- IFF/IFFFile.cs
+++ IFF/IFFFile.cs
@@ -17,6 +17,7 @@ namespace IFF {
             "Course",
             "Club",
             "ClubSet",
+            "Enchant",
             "HairStyle",
             "Title"
         };
