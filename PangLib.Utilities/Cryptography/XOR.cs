cd44b073e0e5945725d6b15e27a9690965bb3205
Andreas N <git@pixelde.su>
2018-11-11 18:50:37 +0100

    Utilities: Document XOR class

--- PangLib.Utilities/Cryptography/XOR.cs
+++ PangLib.Utilities/Cryptography/XOR.cs
@@ -2,13 +2,22 @@
 
 namespace PangLib.Utilities.Cryptography
 {
+    /// <summary>
+    /// Main XOR cipher class
+    /// </summary>
     public static class XOR
     {
+        /// <summary>
+        /// XORs the given bytes with the given key
+        /// </summary>
+        /// <param name="data">Data to be ciphered</param>
+        /// <param name="key">Key to cipher the data with</param>
+        /// <returns>Cuphered data</returns>
         public static byte[] Cipher(byte[] data, uint key)
         {
             for (int i = 0; i < data.Length; i++)
             {
-                data[i] ^= (byte) key;
+                data[i] ^= (byte)key;
             }
 
             return data;
