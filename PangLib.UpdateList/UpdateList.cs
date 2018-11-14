3259fa6b7f8f0e48cbf7e80865ad74ea7a143a09
Andreas N <git@pixelde.su>
2018-11-14 04:22:23 +0100

    UpdateList: Add parsing of UpdateList files

---
+++ PangLib.UpdateList/UpdateList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Xml;
+using PangLib.Utilities.Cryptography;
+
+namespace PangLib.UpdateList
+{
+    public class UpdateList
+    {
+        public string Document;
+
+        private uint[] DecryptionKey;
+        private string FilePath;
+
+        public UpdateList(string filePath)
+        {
+            FilePath = filePath;
+        }
+
+        public void Parse()
+        {
+            if (DecryptionKey == null)
+            {
+                throw new NullReferenceException("DecryptionKey needs to be set using the SetDecryptionKey instance method!");
+            }
+
+            Span<byte> updateList = File.ReadAllBytes(FilePath);
+
+            for (int i = 0; i < updateList.Length; i += 8)
+            {
+                Span<byte> chunk = updateList.Slice(i, 8);
+                Span<uint> decrypted = XTEA.Decipher(16, MemoryMarshal.Cast<byte, uint>(chunk).ToArray(), DecryptionKey);
+                Span<byte> resource = MemoryMarshal.AsBytes(decrypted);
+                resource.CopyTo(chunk);
+            }
+
+            Document = Encoding.UTF8.GetString(updateList.ToArray());
+        }
+
+        public void SetDecryptionKey(uint[] key)
+        {
+            DecryptionKey = key;
+        }
+    }
+}
