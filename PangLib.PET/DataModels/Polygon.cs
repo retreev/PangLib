e78acb2eea4209bc2e9d979f5e604f502bcaead2
Timo <timo@teatwig.net>
2019-05-06 00:06:09 +0200

    Change PETFile fields and all DataModels to be public, Remove BinaryReader and FileDataBytes fields from PETFile, Add "using" statement for auto-disposing of BinaryReaders, Remove unused imports from PangLib.PET

--- PangLib.PET/DataModels/Polygon.cs
+++ PangLib.PET/DataModels/Polygon.cs
@@ -1,9 +1,8 @@
-using System;
 using System.Collections.Generic;
 
 namespace PangLib.PET.DataModels
 {
-    class Polygon
+    public class Polygon
     {
         public List<PolygonIndex> PolygonIndices = new List<PolygonIndex>();
     }
