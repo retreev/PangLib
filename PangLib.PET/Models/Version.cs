25981bc829562d3586e163f099a3aa562044f017
Andreas Nedbal <git@pixelde.su>
2019-05-24 14:20:33 +0200

    PET: Document version structure

--- PangLib.PET/Models/Version.cs
+++ PangLib.PET/Models/Version.cs
@@ -1,10 +1,24 @@
 namespace PangLib.PET.Models
 {
+    /// <summary>
+    /// Base version structure for Puppet files
+    /// </summary>
     public struct Version
     {
+        /// <summary>
+        /// Major version number
+        /// </summary>
         public int Major { get; set; }
+        
+        /// <summary>
+        /// Minor version number
+        /// </summary>
         public int Minor { get; set; }
-
+        
+        /// <summary>
+        /// String override that returns the full version number
+        /// </summary>
+        /// <returns>A string composed of the Major and Minor version number</returns>
         public override string ToString()
         {
             return $"{Major.ToString()}.{Minor.ToString()}";
