0589da327c70af7887d02ed19dc1983ec01ea3b7
Andreas Nedbal <git@pixelde.su>
2019-05-24 14:11:15 +0200

    PET: Document frame structure

--- PangLib.PET/Models/Frame.cs
+++ PangLib.PET/Models/Frame.cs
@@ -1,11 +1,33 @@
 namespace PangLib.PET.Models
 {
+    /// <summary>
+    /// Base frame structure for Puppet files
+    /// </summary>
     public struct Frame
     {
+        /// <summary>
+        /// Index of the frame
+        /// </summary>
         public uint Index { get; set; }
+        
+        /// <summary>
+        /// Script executed on this frame
+        /// </summary>
         public string Script { get; set; }
+        
+        /// <summary>
+        /// X origin coordinate
+        /// </summary>
         public float X { get; set; }
+        
+        /// <summary>
+        /// Y origin coordinate
+        /// </summary>
         public float Y { get; set; }
+        
+        /// <summary>
+        /// Z origin coordinate
+        /// </summary>
         public float Z { get; set; }
     }
 }
