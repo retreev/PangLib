ff2e03b43247e6242bd4091010beabb175d8e314
Andreas Nedbal <git@pixelde.su>
2019-05-24 14:09:01 +0200

    PET: Document bone structure

--- PangLib.PET/Models/Bone.cs
+++ PangLib.PET/Models/Bone.cs
@@ -1,9 +1,26 @@
 namespace PangLib.PET.Models
 {
+    /// <summary>
+    /// Base bone structure for Puppet files
+    /// </summary>
     public struct Bone
     {
+        /// <summary>
+        /// Name of the bone
+        /// </summary>
         public string Name { get; set; }
+        
+        /// <summary>
+        /// ID of the parent bone
+        /// </summary>
+        /// <remarks>
+        /// If this ID is 255 the bone is a root bone
+        /// </remarks>
         public byte Parent { get; set; }
+        
+        /// <summary>
+        /// Perspective projection matrix of the bone
+        /// </summary>
         public float[] Matrix { get; set; }
     }
 }
