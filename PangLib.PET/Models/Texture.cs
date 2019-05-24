d11d58a14b974c72a2fa489916b7acb585b94771
Andreas Nedbal <git@pixelde.su>
2019-05-24 14:20:24 +0200

    PET: Document texture structure

--- PangLib.PET/Models/Texture.cs
+++ PangLib.PET/Models/Texture.cs
@@ -1,7 +1,13 @@
 namespace PangLib.PET.Models
 {
+    /// <summary>
+    /// Base texture structure for Puppet files
+    /// </summary>
     public struct Texture
     {
+        /// <summary>
+        /// File name of the texture
+        /// </summary>
         public string FileName { get; set; }
     }
 }
