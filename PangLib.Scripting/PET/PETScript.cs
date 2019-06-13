af43cf5d56ac24ec4d7b92538cff9da42219b988
Andreas Nedbal <git@pixelde.su>
2019-06-13 23:47:49 +0200

    Scripting: Add initial PETScript tokenizer

---
+++ PangLib.Scripting/PET/PETScript.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using PETToken = PangLib.Scripting.Token<PangLib.Scripting.PET.PETTokenType>;
+using PETTokenDefinition = PangLib.Scripting.TokenDefinition<PangLib.Scripting.PET.PETTokenType>;
+
+namespace PangLib.Scripting.PET
+{
+    public class PETScript
+    {
+        public List<PETToken> Tokens;
+
+        public PETScript(string petScript)
+        {
+            Tokenizer<PETTokenType> tokenizer = new Tokenizer<PETTokenType>(
+                new List<PETTokenDefinition>
+                {
+                    new PETTokenDefinition(PETTokenType.Asterisk, @"^\*"),
+                    new PETTokenDefinition(PETTokenType.StringValue, @"^\w+"),
+                    new PETTokenDefinition(PETTokenType.OpenParenthesis, "^\\("),
+                    new PETTokenDefinition(PETTokenType.Argument, "^\"[^\"]*\"{1}"),
+                    new PETTokenDefinition(PETTokenType.CloseParenthesis, "^\\)")
+                }
+            );
+
+            Tokens = tokenizer.Tokenize(petScript);
+        }
+    }
+}
---
+++ PangLib.Scripting/PET/PETTokenType.cs
@@ -0,0 +1,11 @@
+namespace PangLib.Scripting.PET
+{
+    public enum PETTokenType
+    {
+        Argument,
+        Asterisk,
+        CloseParenthesis,
+        OpenParenthesis,
+        StringValue
+    }
+}
