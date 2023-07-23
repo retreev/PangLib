ea1cd2857daf2745565797e4617fc64f3d4d276c
Andreas Nedbal <git@pixelde.su>
2023-07-23 02:50:40 +0200

    WEP: Add parsing of node lists

---
+++ PangLib.WEP/Helpers/NodeListReader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using PangLib.WEP.Models;
+
+namespace PangLib.WEP.Helpers
+{
+    public class NodeListReader
+    {
+        public static List<NodeList> ReadAllNodeLists(BinaryReader reader, int version, int count)
+        {
+            List<NodeList> nodeLists = new List<NodeList>();
+
+            for (int i = 0; i < count; i++)
+            {
+                NodeList nodeList = new NodeList();
+
+                if (version == 113)
+                {
+                    nodeList.Name = reader.ReadFixedString(16);
+                }
+                else
+                {
+                    nodeList.Name = reader.ReadFixedString(32);
+                }
+
+                nodeList.Length = reader.ReadInt32();
+                nodeList.Type = reader.ReadInt32();
+
+                nodeList.Vectors = new List<Vector3<float>>();
+
+                for (int j = 0; j < nodeList.Length; j++)
+                {
+                    nodeList.Vectors.Add(reader.ReadVector3());
+                }
+                
+                nodeLists.Add(nodeList);
+            }
+
+            return nodeLists;
+        }
+    }
+}
---
+++ PangLib.WEP/Models/NodeList.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace PangLib.WEP.Models
+{
+    public struct NodeList
+    {
+        public string Name { get; set; }
+        public int Length { get; set; }
+        public int Type { get; set; }
+        public List<Vector3<float>> Vectors { get; set; }
+    }
+}
