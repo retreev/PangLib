04da3aae2b7ee5c95e1a2e51073c121bdb6c1654
Andreas Nedbal <git@pixelde.su>
2023-07-23 02:51:55 +0200

    WEP: Add parsing of map checks

---
+++ PangLib.WEP/Helpers/MapCheckReader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using PangLib.WEP.Models;
+
+namespace PangLib.WEP.Helpers
+{
+    public class MapCheckReader
+    {
+        public static MapCheck ReadMapCheck(BinaryReader reader, int version)
+        {
+            MapCheck mapCheck = new MapCheck
+            {
+                Par = reader.ReadByte()
+            };
+
+            if (version == 113)
+            {
+                mapCheck.TeeType = new List<Point>();
+                
+                for (int i = 0; i <= 1; i++)
+                {
+                    mapCheck.TeeType.Add(reader.ReadPoint());
+                }
+
+                mapCheck.PinType1 = new List<Point>();
+                
+                for (int i = 0; i <= 2; i++)
+                {
+                    mapCheck.PinType1.Add(reader.ReadPoint());
+                }
+
+                mapCheck.PinType2 = new List<Point>();
+                
+                for (int i = 0; i <= 2; i++)
+                {
+                    mapCheck.PinType2.Add(reader.ReadPoint());
+                }
+            }
+
+            return mapCheck;
+        }
+    }
+}
---
+++ PangLib.WEP/Models/MapCheck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace PangLib.WEP.Models
+{
+    public struct Point
+    {
+        public float X;
+        public float Z;
+    }
+    
+    public struct MapCheck
+    {
+        public byte Par { get; set; }
+        public List<Point> TeeType { get; set; }
+        public List<Point> PinType1 { get; set; }
+        public List<Point> PinType2 { get; set; }
+    }
+}
