cb9a86bfea0a565ce2f881afd23bd437d1089fe6
Andreas Nedbal <git@pixelde.su>
2023-07-23 02:51:31 +0200

    WEP: Add parsing of textures

---
+++ PangLib.WEP/Helpers/TextureReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace PangLib.WEP.Helpers
+{
+    public class TextureReader
+    {
+        public static List<string> ReadAllTextures(BinaryReader reader, int count)
+        {
+            List<string> textures = new List<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                textures.Add(reader.ReadFixedString(32));
+            }
+
+            return textures;
+        }
+    }
+}
