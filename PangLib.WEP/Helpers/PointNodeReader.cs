df1247342a388920f9462397563fee89e7df2df4
Andreas Nedbal <git@pixelde.su>
2023-07-23 02:48:55 +0200

    WEP: Add parsing of point nodes

---
+++ PangLib.WEP/Helpers/PointNodeReader.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using PangLib.WEP.Models;
+
+namespace PangLib.WEP.Helpers
+{
+    public class PointNodeReader
+    {
+        public static List<PointNode> ReadAllPointNodes(BinaryReader reader, int version, int count)
+        {
+            List<PointNode> pointNodes = new List<PointNode>();
+
+            for (int i = 0; i < count; i++)
+            {
+                PointNode pointNode = new PointNode();
+
+                pointNode.Type = (PointNodeType)reader.ReadByte();
+
+                switch (pointNode.Type)
+                {
+                    case PointNodeType.Start:
+                        pointNode.Name = reader.ReadFixedString(32);
+                        pointNode.Position = reader.ReadVector3();
+
+                        if (version == 114)
+                        {
+                            pointNode.Position2 = reader.ReadVector3();
+                            pointNode.ExtraValues = reader.ReadExtraValues();
+                        }
+
+                        break;
+                    case PointNodeType.Light:
+                    case PointNodeType.Sun:
+                    case PointNodeType.Sequence:
+                    default:
+                        pointNode.Name = reader.ReadFixedString(32);
+                        pointNode.Position = reader.ReadVector3();
+                        pointNode.Data = reader.ReadFixedString(64);
+                        
+                        if (version == 114)
+                        {
+                            pointNode.Position2 = reader.ReadVector3();
+                            pointNode.ExtraValues = reader.ReadExtraValues();
+                        }
+
+                        break;
+                }
+                
+                pointNodes.Add(pointNode);
+            }
+
+            return pointNodes;
+        }
+    }
+}
---
+++ PangLib.WEP/Models/PointNode.cs
@@ -0,0 +1,20 @@
+﻿namespace PangLib.WEP.Models
+{
+    public enum PointNodeType : byte
+    {
+        Start = 0,
+        Light = 1,
+        Sun = 2,
+        Sequence = 3
+    }
+
+    public struct PointNode
+    {
+        public PointNodeType Type { get; set; }
+        public string Name { get; set; }
+        public Vector3<float> Position { get; set; }
+        public string Data { get; set; }
+        public Vector3<float> Position2 { get; set; }
+        public ExtraValues ExtraValues { get; set; }
+    }
+}
