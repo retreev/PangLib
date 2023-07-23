8d58b1944e4463fe8e776aa655ed0ae4ec0c1d0e
Andreas Nedbal <git@pixelde.su>
2023-07-23 02:51:41 +0200

    WEP: Add parsing of colors

---
+++ PangLib.WEP/Helpers/ColorReader.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using PangLib.WEP.Models;
+
+namespace PangLib.WEP.Helpers
+{
+    public class ColorReader
+    {
+        public static List<Color> ReadAllColors(BinaryReader reader, int count)
+        {
+            List<Color> colors = new List<Color>();
+
+            for (int i = 0; i < count; i++)
+            {
+                Color color = new Color();
+                
+                for (int j = 0; j <= 2; j++)
+                {
+                    color.Values.Add(reader.ReadInt32());
+                }
+                
+                colors.Add(color);
+            }
+
+            return colors;
+        }
+
+        public static List<VertexColorMap> ReadAllVertexColorMaps(BinaryReader reader)
+        {
+            List<VertexColorMap> vertexColorMaps = new List<VertexColorMap>();
+            
+            int count = reader.ReadInt32();
+
+            for (int i = 0; i < count; i++)
+            {
+                VertexColorMap vertexColorMap = new VertexColorMap();
+
+                vertexColorMap.Name = reader.ReadPascalString();
+
+                int colorCount = reader.ReadInt32();
+
+                vertexColorMap.Colors = new List<Color>();
+
+                for (int j = 0; j < colorCount; j++)
+                {
+                    Color color = new Color();
+                    int valueCount = reader.ReadInt32();
+
+                    for (int k = 0; k < valueCount; k++)
+                    {
+                        color.Values.Add(reader.ReadInt32());
+                    }
+                    
+                    vertexColorMap.Colors.Add(color);
+                }
+                
+                vertexColorMaps.Add(vertexColorMap);
+            }
+
+            return vertexColorMaps;
+        }
+    }
+}
---
+++ PangLib.WEP/Models/Color.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace PangLib.WEP.Models
+{
+    public struct Color
+    {
+        public List<int> Values { get; set; }
+    }
+
+    public struct VertexColorMap
+    {
+        public string Name { get; set; }
+        public List<Color> Colors { get; set; }
+    }
+}
