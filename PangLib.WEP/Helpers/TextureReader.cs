dee8207a2b73b378b1d3424c9a49a4f0f0d57c9e
Andreas Nedbal <git@pixelde.su>
2023-07-29 06:31:42 +0200

    WEP: Document TextureReader

--- PangLib.WEP/Helpers/TextureReader.cs
+++ PangLib.WEP/Helpers/TextureReader.cs
@@ -3,8 +3,17 @@ using System.IO;
 
 namespace PangLib.WEP.Helpers
 {
+    /// <summary>
+    /// Helper class to read textures from WEP files
+    /// </summary>
     public class TextureReader
     {
+        /// <summary>
+        /// Helper method to read all textures from a WEP file and return a list of them
+        /// </summary>
+        /// <param name="reader">BinaryReader instance</param>
+        /// <param name="count">Count of textures</param>
+        /// <returns>List of textures from the WEP file</returns>
         public static List<string> ReadAllTextures(BinaryReader reader, int count)
         {
             List<string> textures = new List<string>();
