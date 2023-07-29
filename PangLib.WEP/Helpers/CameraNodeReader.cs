3335e4b3ed2cd3ded47d41339347c7c2b79ffcb2
Andreas Nedbal <git@pixelde.su>
2023-07-29 06:29:58 +0200

    WEP: Document CameraNodeReader

--- PangLib.WEP/Helpers/CameraNodeReader.cs
+++ PangLib.WEP/Helpers/CameraNodeReader.cs
@@ -4,8 +4,18 @@ using PangLib.WEP.Models;
 
 namespace PangLib.WEP.Helpers
 {
+    /// <summary>
+    /// Helper class to read <see cref="PangLib.WEP.Models.CameraNode"/> structures from WEP files
+    /// </summary>
     public class CameraNodeReader
     {
+        /// <summary>
+        /// Helper method to read all camera nodes from a WEP file and return a list of them
+        /// </summary>
+        /// <param name="reader">BinaryReader instance</param>
+        /// <param name="version">Version of the WEP file</param>
+        /// <param name="count">Count of camera nodes</param>
+        /// <returns>List of camera nodes from the WEP file</returns>
         public static List<CameraNode> ReadAllCameraNodes(BinaryReader reader, int version, int count)
         {
             List<CameraNode> cameraNodes = new List<CameraNode>();
--- PangLib.WEP/Models/Vector.cs
+++ PangLib.WEP/Models/Vector.cs
@@ -1,10 +1,24 @@
 ﻿namespace PangLib.WEP.Models
 {
+    /// <summary>
+    /// Base vector type
+    /// </summary>
+    /// <typeparam name="T">Number type</typeparam>
     public struct Vector3<T>
     {
+        /// <summary>
+        /// X coordinate value
+        /// </summary>
         public T X { get; set; }
         
+        /// <summary>
+        /// Y coordinate value
+        /// </summary>
         public T Y { get; set; }
+        
+        /// <summary>
+        /// Z coordinate value
+        /// </summary>
         
         public T Z { get; set; }
     }
