afff12f4f467c86e6daab9b798e0cc2e81bc6089
Andreas Nedbal <git@pixelde.su>
2023-08-07 00:23:13 +0200

    SBIN: Implement base SBINFile class

---
+++ PangLib.SBIN/SBINFile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using PangLib.SBIN.Helpers;
+using PangLib.SBIN.Models;
+
+namespace PangLib.SBIN
+{
+    public class SBINFile
+    {
+        public List<PuppetShadowMap> PuppetShadowMaps;
+        public ShadowMap ShadowMap;
+
+        public SBINFile(Stream data)
+        {
+            Parse(data);
+        }
+
+        private void Parse(Stream data)
+        {
+            using (BinaryReader reader = new BinaryReader(data))
+            {
+                int version = reader.ReadInt32();
+
+                if (version > 0xFFFE000)
+                {
+                    PuppetShadowMaps = PuppetShadowMapReader.ReadAllPuppetShadowMaps(reader);
+                }
+                else
+                {
+                    reader.BaseStream.Seek(0, SeekOrigin.Begin);
+                    ShadowMap = ShadowMapReader.ReadShadowMap(reader);
+                }
+            }
+        }
+    }    
+}
