e4245f4454966f2e5af221abf41bcd916baca0b0
Andreas Nedbal <git@pixelde.su>
2023-08-07 00:22:25 +0200

    SBIN: Add parsing of ShadowMaps

---
+++ PangLib.SBIN/Models/ShadowMap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace PangLib.SBIN.Models
+{
+    public class ShadowMap
+    {
+        public List<Vertex> Vertices;
+        public List<Vector3<float>> Mesh3D;
+        public List<Vector2<float>> Mesh2D;
+        public List<Texture> GlobalTextures;
+        public List<Texture> Type0Textures;
+        public List<Texture> Type1Textures;
+        public List<int> GlobalNumberValues;
+        public List<int> Type0NumberValues;
+        public List<int> Type1NumberValues;
+        public List<long> GlobalValues;
+        public List<long> Type0Values;
+        public List<long> Type1Values;
+    }
+}
